8b310c6ccb6fa3aa1060259613dadbc296d7302a
Au2mater <93315669+Au2mater@users.noreply.github.com>
2023-12-06 21:28:02 +0100

    Update __init__.py

--- src/da_en_email_parser_ahmaba/__init__.py
+++ src/da_en_email_parser_ahmaba/__init__.py
@@ -2,7 +2,7 @@
 a_en_email_parser_ahmaba - Python package for parsing and processing English emails.
 """
 
-from .da_en_email_parser import start_pipeline, extract_header, extract_greeting, extract_signature, clean_body, extract_submessage
+from .da_en_email_parser import start_pipeline, extract_header, extract_greeting, extract_signature, clean_body, extract_submessage, drop_fields
 from .resources import test_message
 
 __version__ = "1.0.1"
