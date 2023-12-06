829fd8d336b5381552ac25e26b56b985bf2fe93b
ahmad balkiss <ab.dev@tutamail.com>
2023-12-06 14:26:07 +0100

    added more signature using chatgpt

--- src/da_en_email_parser_ahmaba/resources.py
+++ src/da_en_email_parser_ahmaba/resources.py
@@ -19,8 +19,14 @@ greetings = ['hello', 'hi', 'hey', 'dear'
 
 # Danish and English signatures
 da_sig = ['med[ ]venlig[ ]hilsen','venlig[ ]hilsen','hilsen','de[ ]bedste[ ]hils[e]{0,1}ner'
-        ,'mange[ ]hilsner','vh','kh','mvh','dbh','tak','på[ ]forhånd[ ]tak']
+        ,'mange[ ]hilsner','vh','kh','mvh','dbh','tak','på[ ]forhånd[ ]tak','herligst']
 en_sig = ['sent[ ]by','best[ ]regards','kind[ ]regards','sincerely','regards']
+da_sig += ['venlig[ ]hilsen[ ]fra','venlig[ ]hilsen[ ]og','hilsen[ ]og[ ]god[ ]dag','de[ ]bedste','mange[ ]tak',
+'tak[ ]for[ ]hj[æ]{0,1}lpen','tak[ ]for[ ]sidst','på[ ]gensyn','venligst','venlig[ ]hilsen[ ]og[ ]tak',
+'p[å]{0,1}[ ]gensyn','tak[ ]for[ ]nu','tak[ ]igen','hilsen[ ]fra[ ]mig','mange[ ]hilsner[ ]fra[ ]mig']
+
+en_sig += ['yours[ ]truly','warm[ ]regards','with[ ]regards','with[ ]best[ ]wishes','looking[ ]forward[ ]to',
+'hope[ ]to','best[ ]wishes','thanks','thank[ ]you','thanks[ ]again']
 
 
 greeting_pattern = f"^({'|'.join(greetings)})"+".{0,60}$"
